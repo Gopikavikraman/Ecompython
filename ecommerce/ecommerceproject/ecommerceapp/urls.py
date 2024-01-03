0ae184157ae57ae4c6b903461bd99a152e8e90f8
Gopikavikraman <gopikak252003@gmail.com>
2024-01-03 15:51:48 +0530

    added

---
+++ ecommerce/ecommerceproject/ecommerceapp/urls.py
@@ -0,0 +1,15 @@
+from ecommerceproject import settings
+
+from django.urls import path
+from . import views
+
+from django.conf.urls.static import static
+app_name='ecommerceapp'
+
+urlpatterns = [
+
+    path('',views.allProdCat,name='allProdCat'),
+    path('<slug:c_slug>/', views.allProdCat, name='products_by_category'),
+    path('<slug:c_slug>/<slug:product_slug>/', views.proDetail, name='prodCatdetail')
+
+]
